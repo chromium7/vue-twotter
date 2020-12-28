c9acff488319479f8e06b13495759ccc63bbfc75
Chromium7 <christofersaputra29@gmail.com>
2020-12-28 19:06:14 +0700

    Fix server.js

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const express = require("express");
-const serveStatic = require("serve-static");
-const history = require("connect-history-api-fallback");
-const enforce = require("express-sslify");
+const express = require('express');
+const serveStatic = require('serve-static');
+const history = require('connect-history-api-fallback');
+const enforce = require('express-sslify');
 
 const app = express();
 
-app.use(enforce.HTTPS({ trustProtoHeader: true}));
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(serveStatic(__dirname + '/dist'));
-app.use(history())
+app.use(history());
 
-app.listen(process.ENV.port || 5000);
+app.listen(process.env.PORT || 5000);
